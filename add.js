57ab3f7cb361d4b6cab9ec6011aeecd9ef17e59d
shreyas115 <shre_prakash@yahoo.com>
2022-09-28 14:19:38 +0530

    Addition

---
+++ add.js
@@ -0,0 +1,6 @@
+function add(){
+var num1=document.getElementbyId("firstno").value
+var num2=document.getelementbyId("secondno").value
+var result=num1+num2
+document.getElementbyId("result").value=result
+}
